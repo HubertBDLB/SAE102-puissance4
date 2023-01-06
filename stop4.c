cda8025b64f8d235733f8f9f30892b10b9835843
Maxime P <maxperrot@etudiant.univ-rennes1.fr>
2023-01-06 09:28:45 +0000

    Update stop4.c

--- stop4.c
+++ stop4.c
@@ -5,7 +5,7 @@
  **  Version 1.0                                                             **
  **  A rendre pour le 8 janvier 2023                                         **
  **  Gitlab : https://gitlabiut.iutlan.univ-rennes1.fr/maxperrot/puissance4  **
- **  Fichier : main.c                                                        **
+ **  Fichier : stop4.c                                                       **
  ******************************************************************************/
 
 #include <stdio.h>
